dfddd9b0620750b55aa897d1eb5bec72422d3869
BryanCachimuel <bryanloyo56@gmail.com>
2023-10-02 21:00:49 -0500

    realizando las consultas hacia la base de datos desde el backend

--- backend/furgonetas-server/controllers/mensajeController.js
+++ backend/furgonetas-server/controllers/mensajeController.js
@@ -1,37 +1,47 @@
+const mensajeConsultas = require("../database/consultas/mensajeConsultas");
+
 const mensajeController = {
+  // Metodo enviarMensaje
+  enviarMensaje: async function (req, res) {
+    // Consulta # 1
+    await mensajeConsultas
+      .consultarMensajes()
+      .then((msj) => {
+        console.log("Respuesta de la Consulta #1 a la Base de Datos: ", msj);
+      })
+      .catch((err) => {
+        console.log("Error en la consulta: ", err);
+      });
 
-    // Metodo enviarMensaje
-    enviarMensaje: function (req, res) {
+    // Consulta # 2
+    await mensajeConsultas
+      .consultarMensaje([2])
+      .then((msj) => {
+        console.log("Respuesta de la Consulta #2 a la Base de Datos: ", msj);
 
-      return res.status(200).json(
-        { 
-            result: true,
-            code: 200,
-            mensaje: "Mensaje desde el Back",
-            
-        }
-      ); 
+        // Enviar respuesta al frontend
+        return res.status(200).json({
+          result: true,
+          code: 200,
+          mensaje: msj[0].descripcion_mensaje,
+        });
+      })
+      .catch((err) => {
+        console.log("Error en la consulta: ", err);
+      });
+  },
 
-    },
+  // Metodo recibirMensaje
+  recibirMensaje: function (req, res) {
+    const mensaje_recibido = req.body.mensaje_enviado;
+    console.log("Mensaje desde el Front:", mensaje_recibido);
 
-    // Metodo recibirMensaje
-    recibirMensaje: function (req, res) {
-
-      const mensaje_recibido = req.body.mensaje_enviado
-      console.log("Mensaje desde el Front:", mensaje_recibido)
-
-      return res.status(200).json(
-        { 
-            result: true,
-            code: 200,
-            mensaje: "Se ha recibido el Mensaje",
-            
-        }
-      ); 
-
-    },
-
+    return res.status(200).json({
+      result: true,
+      code: 200,
+      mensaje: "Se ha recibido el Mensaje",
+    });
+  },
 };
-  
+
 module.exports = mensajeController;
-  
