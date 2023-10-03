62f07a716cd90f2037685767ec7f3f09e18b893e
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-03 17:17:03 -0500

    verificando dependencias
    poniendo las dependencias necesarias para el funcionamiento del servidor

--- backend/furgonetas-server/server.js
+++ backend/furgonetas-server/server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const morgan = require('morgan')
 const app = express()
 
 const server = require('http').createServer(app)
 const port = 3002
 
 app.use(cors())
-app.use(morgan("dev"))
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}))
 
