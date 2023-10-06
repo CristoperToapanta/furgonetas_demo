67806f109d181a945d440907502eefddd92d7bc6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-05 19:19:26 -0500

    creando las funciones para el controlador para los casos de insertarMensaje, actualizarMensaje y eliminarMensaje

--- backend/furgonetas-server/controllers/mensajeController.js
+++ backend/furgonetas-server/controllers/mensajeController.js
@@ -11,24 +11,25 @@ const mensajeController = {
       })
       .catch((err) => {
         console.log("Error en la consulta: ", err);
-      });
+      }); 
+  },
 
-    // Consulta # 2
-    await mensajeConsultas
-      .consultarMensaje([2])
-      .then((msj) => {
-        console.log("Respuesta de la Consulta #2 a la Base de Datos: ", msj);
-
-        // Enviar respuesta al frontend
-        return res.status(200).json({
-          result: true,
-          code: 200,
-          mensaje: msj[0].descripcion_mensaje,
-        });
-      })
-      .catch((err) => {
-        console.log("Error en la consulta: ", err);
-      });
+  // Método consultar mensaje por id
+  mensajeporId: async function(req, res) {
+     const id = req.params.id;
+     await mensajeConsultas
+     .consultarMensaje([id])
+     .then((msj) => {
+       console.log("Respuesta de la Consulta #2 a la Base de Datos: ", msj);
+       return res.status(200).json({
+         result: true,
+         code: 200,
+         mensaje: msj[0].descripcion_mensaje,
+       });
+     })
+     .catch((err) => {
+       console.log("Error en la consulta: ", err);
+     });
   },
 
   // Metodo recibirMensaje
@@ -42,6 +43,61 @@ const mensajeController = {
       mensaje: "Se ha recibido el Mensaje",
     });
   },
+
+  // Método para insertar un nuevo mensaje
+  insertarMensaje: async function (req, res){
+    const {descripcion_mensaje}  = req.body;
+    await mensajeConsultas
+      .insertandoMensaje(descripcion_mensaje)
+      .then((msj) => {
+        console.log("Respuesta de la Consulta Insertar: ", msj);
+        return res.status(200).json({
+          result: true,
+          code: 200,
+          mensaje: "Se ha insertado el mensaje",
+        });
+      })
+      .catch((err) => {
+        console.log("Error en la consulta: ", err);
+      });
+  },
+
+  // Método para actualizar un mensaje
+  actualizarMensaje: async function(req, res){
+    const id = req.params.id;
+    const {descripcion_mensaje} = req.body;
+    await mensajeConsultas
+      .actualizarMensaje(descripcion_mensaje, [id])
+      .then((msj) => {
+        console.log("Respuesta de la Consulta Actualizar: ", msj);
+        return res.status(200).json({
+          result: true,
+          code: 200,
+          mensaje: "Se ha actualizado el mensaje",
+        });
+      })
+      .catch((err) => {
+        console.log("Error en la consulta: ", err);
+      });
+  },
+
+  // Método para eliminar un mensaje
+  eliminarMensaje: async function(req, res){
+    const id = req.params.id;
+    await mensajeConsultas
+      .eliminarMensaje([id])
+      .then((msj) => {
+        console.log("Respuesta de la Consulta Eliminar: ", msj);
+        return res.status(200).json({
+          result: true,
+          code: 200,
+          mensaje: "Se ha eliminado el mensaje", msj
+        });
+      })
+      .catch((err) => {
+        console.log("Error en la consulta: ", err);
+      });
+  }
 };
 
 module.exports = mensajeController;
