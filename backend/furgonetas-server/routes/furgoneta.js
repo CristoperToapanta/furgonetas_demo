94a280a4c127b71e71d7a61aefbee551c924da00
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-13 20:10:39 -0500

    estableciendo las rutas las cuales consumen las funciones establecidas en el archivo furgonetaController.js

---
+++ backend/furgonetas-server/routes/furgoneta.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {listarFurgonetas, crear, listadoFurgonetas} = require('../controllers/furgonetaController');
+
+router.route('/listado-furgonetas')
+      .get(
+        (req, res) => {
+            listarFurgonetas(req, res);
+        }
+      );
+
+router.route('/crear-furgoneta')
+        .post(
+            (req, res) => {
+                crear(req, res);
+            }
+        )
+
+module.exports = router;
