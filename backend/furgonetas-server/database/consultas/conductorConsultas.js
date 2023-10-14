a9306c461a482e2ad50a8ddf92eabdada76124f9
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-13 20:03:13 -0500

    creando el archivo conductorConsultas.js en donde se crear los escript con las consultas hacia la base de datos

---
+++ backend/furgonetas-server/database/consultas/conductorConsultas.js
@@ -0,0 +1,23 @@
+const conexion = require('../conexion');
+
+var conductorConsultas = {
+
+    consultarConductores: async function(){
+
+        let query = `SELECT * FROM hypermovilidad.tbl_conductor`;
+
+        return conexion.any(query);
+
+    },
+
+    crearConductor: function(id_furgoneta,cedula_conductor,nombre_conductor,estado_conductor,edad_conductor,tipo_licencia_coductor,direccion_conductor){
+
+        let query = `INSERT INTO ypermovilidad.tbl_conductor(id_furgoneta,cedula_conductor, nombre_conductor,estado_conductor,edad_conductor,tipo_licencia_coductor,direccion_conductor) VALUES('${id_furgoneta}','${cedula_conductor}','${nombre_conductor}','${estado_conductor}',${edad_conductor},'${tipo_licencia_coductor}','${direccion_conductor}')`;
+        
+        return conexion.any(query);
+
+    }
+
+};
+
+module.exports = conductorConsultas;
