344041e40f7677151671dca226fa659dbdaeb42b
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-13 20:09:40 -0500

    estableciendo las rutas las cuales consumen las funciones establecidas en el archivo conductor.js

---
+++ backend/furgonetas-server/routes/conductor.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {listarConductor, crear, listar} = require('../controllers/ConductorController');
+
+router.route('/listado-conductores')
+      .get(
+        (req, res) => {
+            listarConductor(req, res);
+        }
+      );
+
+router.route('/crear-conductor')
+        .post(
+            (req, res) => {
+                crear(req, res);
+            }
+        )
+
+module.exports = router;
