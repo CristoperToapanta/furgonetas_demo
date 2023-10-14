45c5cc71090135f2f9cf077ec2a2e89fb6ef337d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-13 20:07:18 -0500

    creando funciones para consumir las consultas establecidas en conductorConsultas.js

---
+++ backend/furgonetas-server/controllers/ConductorController.js
@@ -0,0 +1,41 @@
+const conductorConsultas = require("../database/consultas/conductorConsultas");
+
+const conductorController = {
+    // Método para listar conductores
+    listarConductor: async function (req, res) {
+        await conductorConsultas
+            .consultarConductores()
+            .then((msj) => {
+                console.log("Respuesta al listado de conductores: ", msj);
+            })
+            .catch((err) => {
+                console.log("Error en la consulta de listado de conductores: ", err);
+            });
+    },
+    
+  /*Método para listar furgonetas
+  listar: async function (req, res) {
+    const listado = await conductorConsultas.consultarConductores();
+    res.json(listado); 
+  },*/
+
+    // Método para crear nuevas conductores
+    crear: async function (req, res) {
+        const { id_furgoneta,cedula_conductor, nombre_conductor,estado_conductor,edad_conductor,tipo_licencia_coductor,direccion_conductor } = req.body;
+        await conductorConsultas
+            .crearConductor(id_furgoneta,cedula_conductor, nombre_conductor,estado_conductor,edad_conductor,tipo_licencia_coductor,direccion_conductor)
+            .then((msj) => {
+                console.log("Respuesta de la Consulta de Creación de conductores: ", msj);
+                return res.status(200).json({
+                    result: true,
+                    code: 200,
+                    mensaje: "Se ha creado un conductor",
+                });
+            })
+            .catch((err) => {
+                console.log("Error en la consulta de creación de conductores: ", err);
+            });
+    }
+};
+
+module.exports = conductorController;
