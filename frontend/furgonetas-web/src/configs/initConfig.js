cce471d4585a0a7b770232c14e55dc8051fa9189
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-17 22:43:08 -0500

    configurando la ip publica con la cual se puede conectar desde el backend

--- frontend/furgonetas-web/src/configs/initConfig.js
+++ frontend/furgonetas-web/src/configs/initConfig.js
@@ -3,7 +3,7 @@ const urlAmbiente = {
     local:
     {
         // Chequear la IP del PC y reemplazar
-        host: 'http://192.168.1.4:3002', // Casa
+        host: 'http://192.168.1.6:3002', // Casa
         // host: 'http://192.168.1.27:3002' // Casa Cable
         // host: 'http://192.168.1.10:3002', // Patio
 
