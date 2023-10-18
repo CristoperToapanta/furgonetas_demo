9bb0d21f7a1c44067f6c25485b0224fc2b928560
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-10-17 22:47:18 -0500

    realizando el procedimiento para la vista de conductores en donde se vera la información de los conductores

--- frontend/furgonetas-web/src/conductor/ConductorScreen.js
+++ frontend/furgonetas-web/src/conductor/ConductorScreen.js
@@ -1,35 +1,34 @@
-import { Typography, Card, CardContent } from '@mui/material';
+import { Box, Typography, Card, CardContent } from '@mui/material';
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const URI = "http://192.168.1.4:3002/prueba/listar/";
+const URI = "http://192.168.1.6:3002/prueba/listado-conductores";
 
 export default function ConductorScreen() {
 
-  const [conductores, setConductores] = useState([])
-
-  useEffect(() => {
-    cargarConductores();
-  },[])
+  const [conductores, setConductores] = useState([]);
 
   const cargarConductores = async () => {
     const response = await axios.get(URI);
     setConductores(response.data);
   };
 
-  
+  useEffect(() => {
+    cargarConductores();
+  },[])
+
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         
         <Typography variant="h5" component="div">
+         
 
-       
         </Typography>
         <Typography variant="body2">
           
           <br />
-          
+          {'"a benevolent smile"'}
         </Typography>
       </CardContent>
     </Card>
