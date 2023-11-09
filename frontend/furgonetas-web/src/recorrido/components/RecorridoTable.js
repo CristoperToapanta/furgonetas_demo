95920f891b34a1109722e65f82ddd1aa8b5cf310
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-11-08 19:46:49 -0500

    organizando adecuadamente los archivos y realizando el consumo del endpoint de consulta de todos los nombres de los conductores

---
+++ frontend/furgonetas-web/src/recorrido/components/RecorridoTable.js
@@ -0,0 +1,66 @@
+import { Table, TableBody, TableContainer, TableHead, TableRow, Paper } from '@mui/material'
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import React, { useEffect, useState } from 'react';
+
+import {consultar_recorrido} from '../../server/recorridoApi';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+export default function RecorridoTable() {
+    const [recorridos, setRecorridos] = useState([])
+
+    useEffect(() => {
+        consultar_recorrido()
+            .then((res) => {
+                console.log("Lista: ", res.data.lista)
+                setRecorridos(res.data.lista)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+      }, [])
+      
+  return (
+    <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+            <TableRow>
+            <StyledTableCell align="center">CONDUCTOR</StyledTableCell>
+            <StyledTableCell align="center">FURGONETA</StyledTableCell>
+            <StyledTableCell align="center">PASAJERO</StyledTableCell>     
+            <StyledTableCell align="center">TIPO RECORRIDO</StyledTableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            {recorridos.map((recorrido) => (
+            <StyledTableRow key={recorrido.id_recorrido}>
+                <StyledTableCell align="center">{recorrido.nombre_conductor}</StyledTableCell>
+                <StyledTableCell align="center">{recorrido.placa_furgoneta}</StyledTableCell>
+                <StyledTableCell align="center">{recorrido.nombre_pasajero}</StyledTableCell>
+                <StyledTableCell align="center">{recorrido.tipo_recorrido}</StyledTableCell>
+            </StyledTableRow>
+            ))}
+        </TableBody>
+        </Table>
+  </TableContainer>
+  )
+}
--- frontend/furgonetas-web/src/server/conductorApi.js
+++ frontend/furgonetas-web/src/server/conductorApi.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const CONSULTAR_CONDUCTORES_URL = initConfig.host + "/conductor/consultar-pasajeros";
 const INSERTAR_CONDUCTOR_URL = initConfig.host + "/conductor/registrar-conductor";
+const NOMBRES_CONDUCTOR_URL = initConfig.host + "/conductor/nombres-conductores";
 
 
 export function consultar_conductores() {
@@ -14,6 +15,12 @@ export function consultar_conductores() {
 export function insertar_conductor(data) {
     return axios.post(`${INSERTAR_CONDUCTOR_URL}`, data, {timeout:50000})
         .then(r=> r) 
+        .catch(err => err)
+}
+
+export function nombres_conductor(){
+    return axios.get(`${NOMBRES_CONDUCTOR_URL}`, {timeout:50000})
+        .then(r=> r)
         .catch(err => err)
 }
 
