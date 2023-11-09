a2b0e9f88716efca309b08d71fcf4b8eb60141cc
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-11-08 20:21:47 -0500

    realizando el endpoint para insertar un nuevo recorrido dentro del backend

--- backend/furgonetas-server/controllers/recorridoController.js
+++ backend/furgonetas-server/controllers/recorridoController.js
@@ -1,6 +1,31 @@
 const recorridoConsultas = require("../database/consultas/recorridoConsultas");
 
 const recorridoController = {
+
+  insertarRecorrido: async function (req, res) {
+    await recorridoConsultas.insertarRecorrido([
+        req.body.id_conductor, 
+        req.body.id_pasajero,
+        req.body.id_furgoneta, 
+        req.body.tipo_recorrido,
+      ])
+      .then((resp) => {
+        console.log(resp)
+        return res.status(200).json({
+          result: true,
+          code: 200,
+          mensaje: 'Se ha registrado un nuevo recorrido: ' + resp[0].id_recorrido,
+          id_recorrido: resp[0].id_recorrido
+        });
+      }).catch((err) => {
+          return res.status(400).json({
+            result: false,
+            code: 400,
+            message: "Ha ocurrido un error en la BDD al insertar un recorrido: " + err
+        });
+      })
+  },
+
   recorridoConsulta: async function (req, res) {
     await recorridoConsultas
       .consultarRecorridos()
--- backend/furgonetas-server/routes/recorrido.js
+++ backend/furgonetas-server/routes/recorrido.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
-const {recorridoConsulta} = require('../controllers/recorridoController');
+const {insertarRecorrido,recorridoConsulta} = require('../controllers/recorridoController');
+
+router.route('/registrar-recorrido')
+    .post((req, res) => {
+        insertarRecorrido(req, res)
+    }
+);
 
 router.route('/consultar-recorridos')
     .get(
         (req, res) =>{
             recorridoConsulta(req, res)
         }
-    )
+);
 
 
 module.exports = router;
