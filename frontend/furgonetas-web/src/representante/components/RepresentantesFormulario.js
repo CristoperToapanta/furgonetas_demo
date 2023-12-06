3aa3fb303bfe70383d0c0e805d5a54ce2327f5da
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-05 22:17:25 -0500

    realizando las validaciones del formulario de Representantes

--- frontend/furgonetas-web/src/representante/components/RepresentantesFormulario.js
+++ frontend/furgonetas-web/src/representante/components/RepresentantesFormulario.js
@@ -22,6 +22,19 @@ export default function RepresentantesFormulario({handleDialogClose,cerrarSideMe
     // llamando al contexto
     const { accionRepresentante } = useContext(AccionContext);
 
+    // validación de los textfields
+    const [errorCedula, setErrorCedula] = useState(false);
+    const [errorNombre, setErrorNombre] = useState(false);
+    const [errorApellido, setErrorApellido] = useState(false);
+    const [errorDireccion, setErrorDireccion] = useState(false);
+    const [errorParentesco, setErrorParentesco] = useState(false);
+
+    const [msgErrorCedula, setMsgErrorCedula] = useState('');
+    const [msgErrorNombre, setMsgErrorNombre] = useState('');
+    const [msgErrorApellido, setMsgErrorApellido] = useState('');
+    const [msgErrorDireccion, setMsgErrorDireccion] = useState('');
+    const [msgErrorParentesco, setMsgErrorParentesco] = useState('');
+
     const handleIngresarRepresentante = (data) => {
         insertar_representante(data)
             .then((res) => {
@@ -39,51 +52,109 @@ export default function RepresentantesFormulario({handleDialogClose,cerrarSideMe
                 
                 <Grid item xs={12}>
                     <TextField
+                        error={errorCedula}
                         label="Cédula"
                         fullWidth
                         value={cedula}
-                        onChange={(e) => setCedula(e.target.value)}
+                        onChange={(e) => {
+                            setCedula(e.target.value)
+                            if(cedula.length >= 10){
+                                setErrorCedula(true);
+                                setMsgErrorCedula("La cédula no debe ser mayor a 10 Digitos");
+                            }else if(isNaN(cedula)){
+                                setErrorCedula(true);
+                                setMsgErrorCedula("Debe Ingresar solo Digitos");
+                            }else{
+                                setErrorCedula(false);
+                                setMsgErrorCedula("");
+                            }
+                        }}
                         margin="normal"
+                        helperText={msgErrorCedula}
                     />
                 </Grid>
 
                 <Grid item xs={6}>
                     <TextField
+                        error={errorNombre}
                         label="Nombre"
                         fullWidth
                         value={nombre}
-                        onChange={(e) => setNombre(e.target.value)}
+                        onChange={(e) => {
+                            setNombre(e.target.value)
+                            if(!isNaN(nombre)){
+                                setErrorNombre(true);
+                                setMsgErrorNombre("Debe ingresar solo letras");
+                            }else{
+                                setErrorNombre(false);
+                                setMsgErrorNombre("");
+                            }
+                        }}
                         margin="normal"
+                        helperText={msgErrorNombre}
                     />
                 </Grid>
 
                 <Grid item xs={6}>
                     <TextField
+                        error={errorApellido}
                         label="Apellido"
                         fullWidth
                         value={apellido}
-                        onChange={(e) => setApellido(e.target.value)}
+                        onChange={(e) => {
+                            setApellido(e.target.value)
+                            if(!isNaN(apellido)){
+                                setErrorApellido(true);
+                                setMsgErrorApellido("Debe ingresar solo letras");
+                            }else{
+                                setErrorApellido(false);
+                                setMsgErrorApellido("");
+                            }
+                        }}
                         margin="normal"
+                        helperText={msgErrorApellido}
                     />
                 </Grid>
 
                 <Grid item xs={6}>
                     <TextField
+                        error={errorDireccion}
                         label="Dirección"
                         fullWidth
                         value={direccion}
-                        onChange={(e) => setDireccion(e.target.value)}
+                        onChange={(e) => {
+                            setDireccion(e.target.value)
+                            if(!isNaN(direccion)){
+                                setErrorDireccion(true);
+                                setMsgErrorDireccion("Debe ingresar solo letras");
+                            }else{
+                                setErrorDireccion(false);
+                                setMsgErrorDireccion("");
+                            }
+                        }}
                         margin="normal"
+                        helperText={msgErrorDireccion}
                     />
                 </Grid>
 
                 <Grid item xs={6}>
                     <TextField
+                        error={errorParentesco}
                         label="Parentesco"
                         fullWidth
                         value={parentesco}
-                        onChange={(e) => setParentesco(e.target.value)}
+                        onChange={(e) => {
+                            setParentesco(e.target.value)
+                            if(!isNaN(parentesco)){
+                                setErrorParentesco(true);
+                                setMsgErrorParentesco("Debe ingresar solo letras");
+                            }else{
+                                setErrorParentesco(false);
+                                setMsgErrorParentesco("");
+                            }
+                        }}
                         margin="normal"
+                        helperText={msgErrorParentesco}
                     />
                 </Grid>   
             </Grid>
